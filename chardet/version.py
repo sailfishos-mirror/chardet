7e25bf40bb4ae6884892c094080e011290494947
Dan Blanchard <dan.blanchard@gmail.com>
2026-03-01 11:04:01 -0500

    Wipe it clean

--- chardet/version.py
+++
@@ -1,9 +0,0 @@
-"""
-This module exists only to simplify retrieving the version number of chardet
-from within setuptools and from chardet subpackages.
-
-:author: Dan Blanchard (dan.blanchard@gmail.com)
-"""
-
-__version__ = "6.0.1.dev0"
-VERSION = __version__.split(".")
